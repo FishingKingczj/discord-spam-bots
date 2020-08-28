4ba94b4ad51d647020da1871a234f301e0cf936f
Czj <453814685@qq.com>
2020-08-28 20:14:04 +0800

    add message dix

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # Basic Config (For all bots)
 pythonCommand = "python" # If you have the python command set to something else such as python3
-userToken = ['fishingkingczj@qq.com:czj199842'] # User Tokens here. example 'token','token' | 'email:pass', 'token'
-DiscordChannel = '' # Channel ID,
+userToken = ['NzQzNzY0NzQ4NDc4MzE2NTQ1.X0jlwA.PWoUjzrd-o9KDWcD1qWIcU6TKrE'] # User Tokens here. example 'token','token' | 'email:pass', 'token'
+DiscordChannel = '713732221873291264' # Channel ID,
 SpamSpeed = 1 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
 textRandom = True # if the root has 'text.txt' it will pick a random text line and send it.
 textFull = False # Will send everything written in 'text.txt'
 
 #Server Joiner
 useTokenJoin = True
-inviteLink = '6c5nDF' # Only the last 6 digits of the invite link https://discord.gg/XXXXXX | Unless you are using email join then just the full URL
+inviteLink = 'SCWKQk' # Only the last 6 digits of the invite link https://discord.gg/XXXXXX | Unless you are using email join then just the full URL
 autojoinServer = False # Will auto join the inviteLink server if True
 useBrowser = 'Chrome' # Chrome | Firefox | Edge (windows only) | Set the browser used for joining the servers.
 joinSpeed = 3 # Changes how fast the bot will start the server joiner
 
 #DM Spam
-DiscordServer = '' # Server ID (https://a.safe.moe/KP4DR.png this one)
+DiscordServer = '713732221873291264' # Server ID (https://a.safe.moe/KP4DR.png this one)
 ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined servers. 
 HeavyScrape = True # (True or False) Use this for servers with a lot of users
 
---
+++ message.py
@@ -0,0 +1,55 @@
+import discord
+import asyncio
+import sys
+import os
+import random
+sys.path.append("./.")
+
+client = discord.Client()
+token = 'NzQzNzY0NzQ4NDc4MzE2NTQ1.X0jlwA.PWoUjzrd-o9KDWcD1qWIcU6TKrE'
+GuidId = '713732221873291264'
+UserList = []
+emojiList = [':smile:', ':laughing:', ':slight_smile:', ':hot_pepper:', ':smirk:']
+enp = []
+
+
+@client.event
+async def on_ready():
+    spam_text = 'null'
+    print("Started Text Spam with " + client.user.name)
+    UserList = []
+    for guild in client.guilds:
+        print(str(guild.id) + ", " + guild.name)
+        if guild.id == GuidId:
+            UserList = guild.member
+            for m in UserList:
+                print(str(m.id) + ", " + m.name)   
+    for member in UserList:
+        if(member.id != client.user.id):
+            if os.path.exists('text.txt'):
+                lines = open('text.txt').read().splitlines()
+                spam_text = random.choice(lines)
+
+            userNames = open('dm_spam_text.txt')
+            text = userNames.read().strip().split()
+            if member.id in text:
+                print(member.name + ' was already messaged')
+            else:
+                try:
+                    await client.send_message(member, spam_text+" "+random.choice(emojiList)+random.choice(emojiList))
+                    print('Sent message to ' + member.name)
+                    file = open('dm_spam_text.txt', 'a')
+                    file.writelines(member.id + '\n')
+                    file.close()
+                    for remaining in range(31, 0, -1):
+                        # Changes how fast the messages are sent. (Discord has a 10 minute cool down for every 10 users)
+                        sys.stdout.write("\r")
+                        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+                        sys.stdout.flush()
+                        await asyncio.sleep(1)
+                    sys.stdout.write("\rComplete!                    \n")
+                except Exception:
+                    print('Something went wrong (;3;) relaunching...')
+
+print('Start running client...')
+client.run(token, bot=False)
